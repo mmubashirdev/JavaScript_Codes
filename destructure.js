a44399fe8132581dcbe3d68aa577f2c641f1aee0
mmubashirdev <muhammadmubashirramzan886@gmail.com.com>
2026-04-21 00:04:43 +0500

    Destruct

--- destructure.js
+++ destructure.js
@@ -3,7 +3,7 @@ let obj = {
   age: 23,
   edu: "BSSE",
 };
-
+//normal way 
 let name = obj.name;
 let age = obj.age;
 let edu = obj.edu;
@@ -11,5 +11,22 @@ let edu = obj.edu;
 console.log("Name: ", name);
 console.log("Age: ", age);
 console.log("eduation: ", edu);
+
+//destructured way
+({name, age, edu} = obj);
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Education: ", edu);
+
+
+let {name: studName, age: StudAge, edu: studEdu} = obj;
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Education: ", edu);
+
+
+let arr = ["Apple", "Ban"]
 
 
