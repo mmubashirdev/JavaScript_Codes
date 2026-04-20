b562aa396a814f95cf4120c80517f01667b90851
mmubashirdev <muhammadmubashirramzan886@gmail.com.com>
2026-04-20 20:58:15 +0500

    Destructuring

---
+++ destructure.js
@@ -0,0 +1,15 @@
+let obj = {
+  name: "Mubashir",
+  age: 23,
+  edu: "BSSE",
+};
+
+let name = obj.name;
+let age = obj.age;
+let edu = obj.edu;
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("eduation: ", edu);
+
+
